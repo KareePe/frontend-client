1976e04f5f7c54fb1f0a7317312c0b6eb7c6c774
KareePe <perapat8589@gmail.com>
2023-11-20 22:00:35 +0700

    config project

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Default</p>
+  </div>
+
+  <slot />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+  <Bar :data="chartData" :options="chartOptions" class="!h-[150px]" />
+
+  <font-awesome-icon icon="fa-brands fa-twitter-square" />
+  <font-awesome-icon icon="fa-brands fa-youtube-square" />
+  <font-awesome-icon icon="fa-brands fa-github-square" />
+</template>
+
+<script setup>
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+import { ref } from "vue";
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+const chartData = ref({
+  labels: ["January", "February", "March", "April", "May"],
+  datasets: [
+    {
+      label: "Data One",
+      backgroundColor: "#f87979",
+      data: [40, 20, 12, 50, 10],
+    },
+  ],
+});
+const chartOptions = ref({
+  responsive: true,
+    maintainAspectRatio: false,
+});
+</script>
+
+<style></style>
