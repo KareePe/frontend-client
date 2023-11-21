34735979ffcbf0a4acfc250a70bf494b9c1dfef6
kuzarandom <peerapat.loeipit@gmail.com>
2023-11-22 00:07:05 +0700

    add apex chart

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
-  <Bar :data="chartData" :options="chartOptions" class="!h-[450px]" />
-
+  <!-- <Bar :data="chartData" :options="chartOptions" class="!h-[450px]" /> -->
+  <client-only placeholder="Loading..." fallback="Loading Chart...">
+    <apexchart
+      type="area"
+      height="400"
+      :options="options"
+      :series="series"
+    ></apexchart>
+  </client-only>
   <v-btn variant="outlined" :loading="true"> Button </v-btn>
 </template>
 
@@ -37,6 +44,65 @@ const chartData = ref({
     },
   ],
 });
+const options = ref({
+  chart: {
+    type: "area",
+    zoom: {
+      enabled: false,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  xaxis: {
+    type: "datetime",
+    labels: {
+      format: "MMM",
+      showDuplicates: false,
+    },
+  },
+  tooltip: {
+    enable: "false",
+  },
+  yaxis: {
+    opposite: true,
+    show: false,
+  },
+  legend: {
+    horizontalAlign: "left",
+  },
+});
+
+const series = ref([
+  {
+    name: "จำนวนเงิน",
+    data: [
+      {
+        x: new Date("2023-07-11").getTime(),
+        y: 76,
+      },
+      {
+        x: new Date("2023-08-12").getTime(),
+        y: 50,
+      },
+      {
+        x: new Date("2023-09-13").getTime(),
+        y: 60,
+      },
+      {
+        x: new Date("2023-10-14").getTime(),
+        y: 20,
+      },
+      {
+        x: new Date("2023-11-15").getTime(),
+        y: 80,
+      },
+    ],
+  },
+]);
 const chartOptions = ref({
   responsive: true,
   maintainAspectRatio: false,
