799b585246a2dfa57eb08e905db8badf70e223b3
kuzarandom <peerapat.loeipit@gmail.com>
2023-11-26 18:12:26 +0700

    [G]: ModalUpdateStock

--- components/Product/ModalUpdateStock.vue
+++ components/Product/ModalUpdateStock.vue
@@ -3,7 +3,7 @@ import { ref, onMounted, watch } from "vue";
 const { open, data, productname } = defineProps({
   open: {
     type: Boolean,
-    default: true,
+    default: false,
   },
   productname: {
     type: String,
@@ -14,13 +14,26 @@ const { open, data, productname } = defineProps({
 });
 const emit = defineEmits(["onSubmit", "onClose"]);
 
-const fnHandleSubmit = () => {
+const fnHandleSubmit = (value) => {
+  console.log(value.target);
+  emit("onSubmit");
+};
+
+const fnHandleClose = () => {
   emit("onClose");
 };
+
+const formValue = ref([
+  {
+    warehouse: null,
+    amount: "",
+  },
+]);
+
 </script>
 
 <template>
-  <v-dialog :model-value="false" persistent width="680px">
+  <v-dialog :model-value="true" persistent width="680px">
     <v-card class="rounded-lg">
       <v-card-text>
         <div class="flex justify-between items-center">
@@ -31,11 +44,7 @@ const fnHandleSubmit = () => {
           </div>
           <v-icon
             size="20"
-            @click="
-              () => {
-                onclose();
-              }
-            "
+            @click="fnHandleClose"
             class="cursor-pointer hover:text-[#084F93] transition-all"
           >
             fa-solid fa-close
@@ -44,51 +53,64 @@ const fnHandleSubmit = () => {
         <div class="text-subTitle opacity-detail leading-5 tracking-23 mt-1">
           อัพเดทจำนวนสินค้าที่อยู่ในสต็อค
         </div>
-        <v-form @submit.prevent="(e) => {}">
-          <div>
-            <div class="flex gap-2 mt-4 items-center w-full">
-              <div
-                class="w-32 text-subTitle leading-[22px] font-[600] text-black opacity-title tracking-31"
-              >
-                คลัง
+        <v-form @submit.prevent="fnHandleSubmit">
+          <div class="max-h-[350px] overflow-y-auto">
+            <div v-for="(item, index) in formValue" :key="index">
+              <div class="flex gap-2 mt-4 items-center w-full">
+                <div
+                  class="w-32 text-subTitle leading-[22px] font-[600] text-black opacity-title tracking-31"
+                >
+                  คลัง
+                </div>
+                <div class="text-subTitle leading-5 tracking-23 w-full">
+                  <v-select
+                    label="เลือกคลัง"
+                    v-model="formValue[index].warehouse"
+                    :items="[
+                      'คลัง 1',
+                      'คลัง 2',
+                      'คลัง 3',
+                      'คลัง 4',
+                      'คลัง 5',
+                      'คลัง 6',
+                    ]"
+                    density="compact"
+                    hide-details
+                    variant="outlined"
+                    class="w-full preinner-center"
+                  ></v-select>
+                </div>
               </div>
-              <div class="text-subTitle leading-5 tracking-23 w-full">
-                <v-select
-                  label="เลือกคลัง"
-                  :items="[
-                    'คลัง 1',
-                    'คลัง 2',
-                    'คลัง 3',
-                    'คลัง 4',
-                    'คลัง 5',
-                    'คลัง 6',
-                  ]"
-                  density="compact"
-                  hide-details
-                  variant="outlined"
-                  class="w-full preinner-center"
-                ></v-select>
-              </div>
-            </div>
 
-            <div class="flex gap-2 mt-4 items-center w-full">
-              <div
-                class="w-32 text-subTitle leading-[22px] font-[600] text-black opacity-title tracking-31"
-              >
-                จำนวน
-              </div>
-              <div class="text-subTitle leading-5 tracking-23 w-full">
-                <v-text-field
-                  variant="outlined"
-                  density="compact"
-                  hide-details
-                  class="w-full preinner-center"
-                ></v-text-field>
+              <div class="flex gap-2 mt-4 items-center w-full">
+                <div
+                  class="w-32 text-subTitle leading-[22px] font-[600] text-black opacity-title tracking-31"
+                >
+                  จำนวน
+                </div>
+                <div class="text-subTitle leading-5 tracking-23 w-full">
+                  <v-text-field
+                    variant="outlined"
+                    density="compact"
+                    hide-details
+                    v-model="formValue[index].amount"
+                    class="w-full preinner-center"
+                  ></v-text-field>
+                </div>
               </div>
             </div>
           </div>
-
-          <v-btn class="mt-2" variant="text" color="#74777F">
+          <v-btn
+            class="mt-2"
+            variant="text"
+            color="#74777F"
+            @click="
+              formValue.push({
+                warehouse: null,
+                amount: '',
+              })
+            "
+          >
             เพิ่มคลัง
             <template v-slot:prepend>
               <v-icon color="#74777F" size="16" class=""
@@ -102,7 +124,7 @@ const fnHandleSubmit = () => {
               class="rounded-lg w-[calc(50%-16px)]"
               color="#084F93"
               variant="outlined"
-              @click="() => onclose()"
+              @click="fnHandleClose"
               >ยกเลิก</v-btn
             >
             <v-btn
